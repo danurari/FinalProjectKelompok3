4ce89d87c14a4c199d38e4b47bdee98098d080b7
Abdurrahman <abdurwakilnyajihad@gmail.com>
2026-03-11 22:36:54 +0700

    konfigurasi minIO

--- django-app/bookstorage/settings.py
+++ django-app/bookstorage/settings.py
@@ -95,7 +95,16 @@ DATABASES = {
 
 AWS_ACCES_KEY_ID = get_secret('minio_access_key', 'minioadmin')
 AWS_SECRET_ACCESS_KEY = get_secret('minio_secret_key', 'password')
+AWS_STORAGE_BUCKET_NAME = 'bookstorage-bucket'
+AWS_S3_ENDPOINT_URL = 'http://minio:9000'
 
+AWS_S3_REGION_NAME = 'us-east-1'             
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_ADDRESSING_STYLE = 'path'             
+AWS_S3_FILE_OVERWRITE = False                
+AWS_DEFAULT_ACL = 'public-read'              
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
