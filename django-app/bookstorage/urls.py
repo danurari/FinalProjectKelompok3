0c1c916c8f8d370197d1a61f64115e9684ca9e06
jek952 <aldzakky0@gmail.com>
2026-04-03 16:26:20 +0700

    feat: menambahkan fitur nginx dan monitoring

--- django-app/bookstorage/urls.py
+++ django-app/bookstorage/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from buku import views
 
 urlpatterns = [
+    path('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
     path('catalog/', views.catalog, name='catalog'),       # Alamat: /catalog/
-    path('/add/', views.add_book, name='add_book'),         # Alamat: /add/
+    path('/add/', views.add_book, name='add_book'),         # Alamat: /add/           
 ]
