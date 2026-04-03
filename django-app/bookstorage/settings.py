aeee39b353d538686caf48206bb4268c0a9b75f6
Abdurrahman <abdurwakilnyajihad@gmail.com>
2026-04-03 18:35:48 +0700

    feat: menambahkan docker-stack.yml untuk deploy stack

--- django-app/bookstorage/settings.py
+++ django-app/bookstorage/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'bookstorage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'namadb_default'),
+            'USER': os.environ.get('POSTGRES_USER', 'user_default'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'pass_default'),
+            'HOST': os.environ.get('DB_HOST', 'database-postgres'),
+            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
